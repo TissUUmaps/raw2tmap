c768bdc9c84dcb6aa29bfc199c0871d81f41ec71
Jonas Windhager <jonas.windhager@uzh.ch>
2023-06-30 20:32:06 +0200

    drop support for piping

--- raw2tmap/__main__.py
+++ raw2tmap/__main__.py
@@ -1,6 +1,4 @@
-import sys
 from pathlib import Path
-from typing import BinaryIO
 
 import click
 
@@ -8,16 +6,19 @@ from raw2tmap import convert_raw_to_tmap
 
 
 @click.command(name="raw2tmap", help="Convert OME-NGFF to TMAP.")
-@click.argument("raw_file", type=click.File(mode="rb"))
-@click.option(
-    "-o",
+@click.argument(
+    "raw_file",
+    type=click.Path(exists=True, path_type=Path),
+    help="Path to input OME-NGFF file.",
+)
+@click.argument(
     "tmap_file",
     type=click.Path(dir_okay=False, writable=True, path_type=Path),
     help="Path to output TMAP file.",
 )
 @click.version_option()
-def main(raw_file: BinaryIO, tmap_file: Path | None) -> None:
-    convert_raw_to_tmap(raw_file, tmap_file or sys.stdout.buffer)
+def main(raw_file: Path, tmap_file: Path) -> None:
+    convert_raw_to_tmap(raw_file, tmap_file)
 
 
 if __name__ == "__main__":
--- raw2tmap/raw2tmap.py
+++ raw2tmap/raw2tmap.py
@@ -1,18 +1,8 @@
 from os import PathLike
 from pathlib import Path
-from typing import BinaryIO
 
 
-def convert_raw_to_tmap(
-    raw_file: str | PathLike | BinaryIO,
-    tmap_file: str | PathLike | BinaryIO,
-) -> None:
-    if isinstance(raw_file, (str, PathLike)):
-        with Path(raw_file).open("rb") as raw_file:
-            return convert_raw_to_tmap(raw_file, tmap_file)
-    assert isinstance(raw_file, BinaryIO)
-    if isinstance(tmap_file, (str, PathLike)):
-        with Path(tmap_file).open("wb") as tmap_file:
-            return convert_raw_to_tmap(raw_file, tmap_file)
-    assert isinstance(tmap_file, BinaryIO)
+def convert_raw_to_tmap(raw_file: str | PathLike, tmap_file: str | PathLike) -> None:
+    raw_file = Path(raw_file)
+    tmap_file = Path(tmap_file)
     raise NotImplementedError()  # TODO
