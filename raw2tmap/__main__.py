95b3493909f4c13164afe7c2b89fdb0bfec61683
Jonas Windhager <jonas.windhager@uzh.ch>
2023-06-30 20:43:56 +0200

    fix CLI

--- raw2tmap/__main__.py
+++ raw2tmap/__main__.py
@@ -6,16 +6,8 @@ from raw2tmap import convert_raw_to_tmap
 
 
 @click.command(name="raw2tmap", help="Convert OME-NGFF to TMAP.")
-@click.argument(
-    "raw_file",
-    type=click.Path(exists=True, path_type=Path),
-    help="Path to input OME-NGFF file.",
-)
-@click.argument(
-    "tmap_file",
-    type=click.Path(dir_okay=False, writable=True, path_type=Path),
-    help="Path to output TMAP file.",
-)
+@click.argument("raw_file", type=click.Path(exists=True, path_type=Path))
+@click.argument("tmap_file", type=click.Path(path_type=Path))
 @click.version_option()
 def main(raw_file: Path, tmap_file: Path) -> None:
     convert_raw_to_tmap(raw_file, tmap_file)
